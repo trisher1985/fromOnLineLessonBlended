9fb3e0f80bc488e4363187ddd6fc6988c7490d6e
YuraHez <trisher1985@gmail.com>
2025-06-28 18:59:45 +0300

    added ended3 fixes

--- app/@modal/(.)posts/[id]/page.tsx
+++ app/@modal/(.)posts/[id]/page.tsx
@@ -1,34 +1,37 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import PostDetailsClient from './PostPreview.client';
 import { fetchPostById } from '@/lib/api';
+import { Metadata } from 'next';
 
-type ParamsType = {
-  id: string;
-};
+interface PostDetailProps {
+  params: Promise<{ id: string }>;
+}
 
-type PageProps = {
-  params: ParamsType;
-  searchParams?: { [key: string]: string | string[] | undefined };
-};
+export async function generateMetadata({ params }: PostDetailProps): Promise<Metadata> {
+  const { id } = await params;
+  const parsedId = Number(id);
+  const post = await fetchPostById(parsedId);
 
-export default async function PostPreview({ params }: PageProps) {
-  const { id } = params;
+  return {
+    title: post.title,
+    description: `${post.body.slice(0, 21)}...`,
+  };
+}
+
+export default async function PostDetails({ params }: PostDetailProps) {
+  const { id } = await params;
   const parsedId = Number(id);
 
   const queryClient = new QueryClient();
-  
-  try {
-    await queryClient.prefetchQuery({
-      queryKey: ['post', parsedId],
-      queryFn: () => fetchPostById(parsedId),
-    });
-  } catch (error) {
-    console.error('Failed to prefetch post:', error);
-  }
+
+  await queryClient.prefetchQuery({
+    queryKey: ['post', parsedId],
+    queryFn: () => fetchPostById(parsedId),
+  });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      
+      <PostPreview.client/>
     </HydrationBoundary>
   );
 }
-
